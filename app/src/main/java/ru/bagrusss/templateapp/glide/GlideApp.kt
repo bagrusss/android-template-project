e84aa2d5ec1ea093ffee433bc303d41143eeaf93
bagrusss <bagrusss@gmail.com>
2020-01-19 06:10:37 +0300

    glide for images

---
+++ app/src/main/java/ru/bagrusss/templateapp/glide/GlideApp.kt
@@ -0,0 +1,11 @@
+package ru.bagrusss.templateapp.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by bagrusss on 19.01.2020
+ */
+
+@GlideModule
+class GlideApp : AppGlideModule()

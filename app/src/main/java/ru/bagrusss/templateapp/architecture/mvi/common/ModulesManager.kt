18a8bca0ccfe0a7fe8208fb9ebdf6261270d1a3d
bagrusss <>
2020-10-24 15:45:19 +0300

    module's components

---
+++ app/src/main/java/ru/bagrusss/templateapp/architecture/mvi/common/ModulesManager.kt
@@ -0,0 +1,3 @@
+package ru.bagrusss.templateapp.architecture.mvi.common
+
+interface ModuleInitArgs
---
+++ app/src/main/java/ru/bagrusss/templateapp/screens/mvi_example/di/DemoModule.kt
@@ -0,0 +1,6 @@
+package ru.bagrusss.templateapp.screens.mvi_example.di
+
+import dagger.Module
+
+@Module
+interface DemoModule

5fb4b9a051dd10456993c89e0fe85d19554cfd48
bagrusss <>
2020-12-27 02:44:44 +0300

    plugin project

---
+++ mvi_arch_plugin/src/main/kotlin/ru/bagrusss/arch/mvi/plugin/actions/CreateFragmentAction.kt
@@ -0,0 +1,11 @@
+package ru.bagrusss.arch.mvi.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class CreateFragmentAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+
+}
